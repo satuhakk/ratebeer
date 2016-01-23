808d3295cfbb7a9441408788c5cdc514e009654d
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-23 20:28:13 +0200

    Ratings-sivulla lista oluista arvosanoineen

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,6 +2,7 @@ class RatingsController < ApplicationController
 
   def index
     @ratings = Rating.all
+    @beers = Beer.all
     # tässä renderöidään oletuksena näkymätemplate /app/views/ratings/index.html
   end
 
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,14 @@
 class Brewery < ActiveRecord::Base
   has_many :beers
+
+  def print_report
+    puts self
+    puts "Established in #{self.year}"
+    puts "Number of beers: #{self.beers.count}"
+  end
+
+  def restart
+    self.year = 2016
+    puts "changed year to #{year}"
+  end
 end
