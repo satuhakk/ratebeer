75961ce9214373882dea6faf84e60cbb548adc64
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-25 12:01:22 +0200

    Arvostelujen keskiarvo näkyy olutsivulla

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    #Etsitään olutta koskevat arvostelut:
+    @ratings = Rating.all
+    #kaikki = @ratings.where beer_id:self.id
+
+    #Summataan arvot:
+    #kaikki = @ratings.where(beer_id:self.id).inject(0){|sum, item| sum + item.score}
+
+    #Keskiarvo:
+    keskiarvo = @ratings.where(beer_id:self.id).inject(0){|sum, item| sum + item.score}.to_f / @ratings.where(beer_id:self.id).length
+
+  end
+
 end
