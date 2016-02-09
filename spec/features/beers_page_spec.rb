dbbcf461809a98631a97a17c314c37053f6b16ad
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-02-09 21:13:06 +0200

    Beers-page -testien korjaus

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 describe "Beers page" do
-  let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
 
-  it "should add a beer when the name is valid" do
-    visit new_beer_path
-    fill_in('beer_name', with:'Hardcore IPA')
-    expect{
+  describe "when signed in" do
+
+    let!(:user) { FactoryGirl.create :user }
+    let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+
+    it "should add a beer when the name is valid" do
+      sign_in(username:"Pekka", password:"Foobar1")
+      visit new_beer_path
+      fill_in('beer_name', with:'Hardcore IPA')
+      expect{
+        click_button('Create Beer')
+      }.to change{Beer.count}.by(1)
+    end
+
+    it "should show an error message if the name is not valid" do
+      sign_in(username:"Pekka", password:"Foobar1")
+      visit new_beer_path
       click_button('Create Beer')
-    }.to change{Beer.count}.by(1)
-  end
+      expect(page).to have_content 'Name is too short (minimum is 1 character)'
+    end
 
-  it "should show an error message if the name is not valid" do
-    visit new_beer_path
-    click_button('Create Beer')
-    expect(page).to have_content 'Name is too short (minimum is 1 character)'
   end
 
 end
