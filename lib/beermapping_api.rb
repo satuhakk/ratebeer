bdc316eac9beefe32c5e9cf3334459f67f80558f
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-02-19 15:50:36 +0200

    Cachen käyttöönotto

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,12 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 24.hours) { fetch_places_in(city) }
+  end
+
+  private
+
+  def self.fetch_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
