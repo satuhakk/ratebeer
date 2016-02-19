ca470fece04da26c8e8bd83dd41d8309a264465e
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-02-19 13:40:14 +0200

    Lisätty BeermappingApin testejä

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do | place |
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    "d40ba50221a4051f7c445cd15ac84736"
+  end
+end
